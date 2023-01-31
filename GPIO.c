e30adcd7e612101057dd857fd7d75302f4f8019a
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-31 22:42:13 +0600

    main file updated

--- GPIO.c
+++ GPIO.c
@@ -3,13 +3,19 @@
 
 void GPIO_WritePin(GPIO_TypeDef *GPIOx,uint16_t GPIO_pin,GPIO_PinState PinState)
 {
-// You need to implement
+		if (PinState==0)
+			GPIOx->BSRR |= (1<<GPIO_pin);
+		else
+			GPIOx->BSRR |= ((1<<GPIO_pin)<<16);
 }
-
 void GPIO_Init(GPIO_TypeDef* GPIOx,GPIO_InitTypeDef *GPIO_Init)
 {
 // You need to implement this function	
 
+		RCC->AHB1ENR |= (1<<0);
+	
+		GPIOx->MODER |= (1<<14);
+		
+		GPIOx->OTYPER = 0;
+		GPIOx->OSPEEDR = 0;
 }
-
-
